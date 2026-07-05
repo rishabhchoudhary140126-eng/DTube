d5873fb582a8cb109f7482980b00533126eb0d6a
Arihant Dev <arihant@Arihants-MacBook-Air.local>
2026-07-06 01:41:59 +0530

    login and signup done

--- server.js
+++ server.js
@@ -54,15 +54,38 @@ app.post("/signup", upload.single("profile_pic"), function(req, res){
     fs.writeFileSync("./data/users.json", JSON.stringify(users, null, 2));
 
 
-
-
     res.send("Signup successful");
-
-
-
-
 })
 
+
+function giveCurrentUser(req, res, next){
+    let currentUser = null;
+    const email = req.body.email;
+    const password = req.body.password;
+    console.log(email, "\n", password);
+    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+
+    for(let i=0; i<users.length; i++){
+        if(users[i].email === email){
+            if(users[i].password === password){
+                req.currentUser = users[i];
+                return next();
+            }
+            else return res.send("Invalid password");
+        }
+    }
+    return res.send("User not found");
+}
+
+
+app.post("/login", upload.none(), giveCurrentUser, function(req, res){
+    res.send("Login successful");
+});
+
+
+app.post("/upload", upload.fields([{ name : "video", maxCount: 1 }, { name : "thumbnail", maxCount: 1 }]),giveCurrentUser,  function(req, res){
+     
+});
 
 
 app.listen(3000);
