c5c5e9bd64d169106c3d46bd00ab58e2ce6cf76d
Rishabh Choudhary <rishabhchoudhary140126@gmail.com>
2026-07-18 12:48:23 +0530

    whole project files restructured

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,19 @@ const app = express();
 app.use(express.json());
 const cors = require('cors'); 
 app.use(cors());
-app.use(express.static(__dirname));
 const fs = require("fs");
 const multer = require("multer");
 const path = require("path");
 
+app.use(express.static(path.join(__dirname, "../frontend")));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/data/profile_pics", express.static(path.join(__dirname, "data/profile_pics")));
+
+
 const storage_profilePic = multer.diskStorage({
 
     destination: function(req, file, cb){
-        cb(null, "./data/profile_pics");
+        cb(null, path.join(__dirname, "data", "profile_pics"));
     },
 
     filename: function(req, file, cb){
@@ -42,7 +46,7 @@ app.post("/signup", upload_profilePic.single("profile_pic"), function(req, res){
     }
     if(!curentUser.email.endsWith("@gmail.com")) return res.status(403).send("Only gmail address allowed!!!")
 
-    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "users.json"), "utf8"));
 
     // console.log(req.body);
     console.log(curentUser);
@@ -54,7 +58,7 @@ app.post("/signup", upload_profilePic.single("profile_pic"), function(req, res){
     }
 
     users.push(curentUser);
-    fs.writeFileSync("./data/users.json", JSON.stringify(users, null, 2));
+    fs.writeFileSync(path.join(__dirname, "data", "users.json"), JSON.stringify(users, null, 2));
 
 
     res.send("Signup successful");
@@ -65,7 +69,7 @@ function giveCurrentUser(req, res, next){
     let currentUser = null;
     const email = req.headers.email;
     const password = req.headers.password;
-    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "users.json"), "utf8"));
 
     for(let i=0; i<users.length; i++){
         if(users[i].email === email){
@@ -90,8 +94,9 @@ app.post("/login", giveCurrentUser, function(req, res){
 const storage_upload = multer.diskStorage({
     destination: function(req, file, cb){
         const user = req.currentUser.email.replaceAll(".", "_").replaceAll("@", "_");
-        fs.mkdirSync('./uploads/' + user, { recursive: true });
-        cb(null, "./uploads/" + user);
+        const uploadPath = path.join(__dirname, "uploads", user);
+        fs.mkdirSync(uploadPath, { recursive: true });
+        cb(null, uploadPath);
     },
     filename: function(req, file, cb){
 
@@ -118,9 +123,10 @@ app.post("/upload", giveCurrentUser, upload_upload.fields([{ name : "video", max
      }
 
     const user = req.currentUser.email.replaceAll(".", "_").replaceAll("@", "_");
-    fs.writeFileSync("./uploads/" + user + "/" + req.currentUser.uploadCount + ".json", JSON.stringify(metaData, null, 2));
+    const uploadPath = path.join(__dirname, "uploads", user);
+    fs.writeFileSync(path.join(uploadPath, `${req.currentUser.uploadCount}.json`), JSON.stringify(metaData, null, 2));
     
-    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data" , "users.json"), "utf8"));
     let currentIndex = 0;
     for(let i=0; i<users.length; i++){
         if(users[i].email === req.currentUser.email){
@@ -130,7 +136,7 @@ app.post("/upload", giveCurrentUser, upload_upload.fields([{ name : "video", max
     }
 
     users[currentIndex].uploadCount++;
-    fs.writeFileSync("./data/users.json", JSON.stringify(users, null, 2));
+    fs.writeFileSync(path.join(__dirname, "data", "users.json"), JSON.stringify(users, null, 2));
 
 
     res.send("Upload successful");
@@ -138,13 +144,14 @@ app.post("/upload", giveCurrentUser, upload_upload.fields([{ name : "video", max
 
 
 app.get("/videos", giveCurrentUser, function(req, res){
-    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "users.json"), "utf8"));
     const videos = [];
     let currentIndex = 0;
     for(let i=0; i<users.length; i++){
         for(let j=1; j<users[i].uploadCount; j++){
             let email = users[i].email.replaceAll(".", "_").replaceAll("@", "_");
-            let metadata = JSON.parse(fs.readFileSync("./uploads/" + email + "/" + j + ".json", "utf8"));
+            let filePathh = j + ".json";
+            let metadata = JSON.parse(fs.readFileSync(path.join(__dirname, "uploads", email, filePathh), "utf8"));
             videos.push({
                 title: metadata.title,
                 description: metadata.description,
@@ -159,7 +166,7 @@ app.get("/videos", giveCurrentUser, function(req, res){
 });
 
 app.get("/video", giveCurrentUser, function(req, res){
-    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "users.json"), "utf8"));
     let channelName = " ";
     const videoPath = req.query.videoPath;
     const emailFolderName = videoPath.split("/")[2];
@@ -174,8 +181,8 @@ app.get("/video", giveCurrentUser, function(req, res){
 
     console.log(videoPath);
     const metadataPath = videoPath.replaceAll(".mp4", ".json");
-    const metadata = JSON.parse(fs.readFileSync("." + metadataPath, "utf8"));
-    const channelLogoPath = "./data/profile_pics/" + emailFolderName + ".jpg";
+    const metadata = JSON.parse(fs.readFileSync(path.join(__dirname, metadataPath), "utf8"));
+    const channelLogoPath = "/data/profile_pics/" + emailFolderName + ".jpg";
     const ans = {
         path: videoPath,
         title: metadata.title,
@@ -189,11 +196,11 @@ app.get("/video", giveCurrentUser, function(req, res){
 
 app.get("/metadata", giveCurrentUser, function(req, res){
     let email = req.headers.email;
-    let upload_number = req.headers.upload_number;
+    let upload_number = req.headers.upload_number + ".json";
 
     email = email.replaceAll(".", "_").replaceAll("@", "_");
 
-    const metadata_path = "./uploads/" + email + "/" + upload_number +".json";
+    const metadata_path = path.join(__dirname, "uploads", email, upload_number);
     const metadata = JSON.parse(fs.readFileSync(metadata_path, "utf8"));
     const title = metadata.title;
     res.send({
@@ -203,7 +210,7 @@ app.get("/metadata", giveCurrentUser, function(req, res){
 });
 
 app.get("/profile", giveCurrentUser, function(req, res){
-    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "users.json"), "utf8"));
     let email = req.headers.email;
     let password = req.headers.password;
     let currentIndex = -1;
@@ -232,7 +239,7 @@ app.get("/profile_pic", giveCurrentUser, function(req, res){
 });
 
 app.post("/userSubscribed", giveCurrentUser, function(req, res){
-    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "users.json"), "utf8"));
     const videoPath = req.query.videoPath;
     const emailFolderName = videoPath.split("/")[2];
     const channelEmail = emailFolderName.replaceAll("_gmail_com", "@gmail.com");
@@ -251,12 +258,12 @@ app.post("/userSubscribed", giveCurrentUser, function(req, res){
         }
     }
     console.log("One user subscribed!!");
-    fs.writeFileSync("./data/users.json", JSON.stringify(users, null, 2));
+    fs.writeFileSync(path.join(__dirname, "data", "users.json"), JSON.stringify(users, null, 2));
     res.status(200).send("Subscribed!");
 });
 app.get("/viewCount", giveCurrentUser, function(req, res){
     let videoPath = req.query.videoPath;
-    videoPath = "." + videoPath.replaceAll(".mp4", ".json");
+    videoPath = path.join(__dirname, videoPath.replaceAll(".mp4", ".json"));
     console.log(videoPath);
     const videoMeta = JSON.parse(fs.readFileSync(videoPath , "utf8"));
     videoMeta.views++;
@@ -270,25 +277,25 @@ app.get("/channelPage", giveCurrentUser, function(req, res){
     const channelPath = channelEmail.replaceAll(".", "_").replaceAll("@", "_");
     console.log(channelPath);
     let currentIndex = -1;
-    const users = JSON.parse(fs.readFileSync("./data/users.json", "utf8"));
+    const users = JSON.parse(fs.readFileSync(path.join(__dirname, "data", "users.json"), "utf8"));
     for(let i=0; i<users.length; i++){
         if(users[i].email == channelEmail){
             currentIndex = i;
             break;
         }
     }
-    const profile_pic_path = "./data/profile_pics/" + channelEmail.replaceAll(".", "_").replaceAll("@", "_") + ".jpg";
+    const profile_pic_path = "/data/profile_pics/" + channelEmail.replaceAll(".", "_").replaceAll("@", "_") + ".jpg";
     const channelName = users[currentIndex].channelName;
 
 
     const numberOfUploads = users[currentIndex].uploadCount;
     let videos = [];
     for(let i = 1; i<numberOfUploads; i++){
-        const videoMeta = JSON.parse(fs.readFileSync(`./uploads/${channelPath}/${i}.json`, "utf8"));
+        const videoMeta = JSON.parse(fs.readFileSync(`uploads/${channelPath}/${i}.json`, "utf8"));
         videos.push({
             title: videoMeta.title,
-            thumbnailPath : `./uploads/${channelPath}/${i}.jpg`,
-            videoPath : `/uploads/${channelPath}/${i}.mp4`
+            thumbnailPath : `uploads/${channelPath}/${i}.jpg`,
+            videoPath : `uploads/${channelPath}/${i}.mp4`
             
         })
     }
